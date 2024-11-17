f897a9eed6e9d6c9bc669010e01983afaa9c21dc
Jeff Yarbrough <jyarbroughiv@gmail.com>
2024-11-16 16:49:29 -0800

    jyarbrough_Hour16
    Hour 16 Particle Effects

---
+++ jyarbrough_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,13 @@
+/*
+I was going for a blaster blaster bolt. 
+I made changes to 6 different aspects, and would likely make other changes before implementing. I would actually create a blaster bolt sprite to use in place of the standard particle
+
+ChangeS made:
+-Scale X,Y changed to represent a blaster bolt
+-Start speed increased to 100 
+-Lifetime decreased to 1
+-Color changed to blue (Ion blaser...great against droids) 
+-Shape disabled
+-Rate over time turned down to 2 (2 bolts per second)
+    -if used in a game, would change this to 1 and have an option to switch to 3 rolt burst)
+ */
--- jyarbrough_Hour14and16/Assets/UserInterface.cs
+++ jyarbrough_Hour14and16/Assets/UserInterface.cs
@@ -1,11 +1,23 @@
 /* 
  Modifications made:
+Title added
+Background changed to a screenshot from a past project that could serve as the basis of a future project. 
+Custom font added with different variations created for different us cases
+Options menu added - presently no functionality. Buttons work to leave menu
+    -Sound On/Off
+    -SFX On/Off
 
-Background changed
-Custom font added
+"Quit" button added to main menu as well 
 
-Health bar addded
- 
+Method for quitting the game (can only be done with an actual build) added to LevelManager.cs
+	
+public void QuitGame()
+    {
+		Application.Quit();
+    }
+
+While not added in this project, I started to create (sketch) a UI for the player with a health bar, a toolbelt, and a countdown timer placeholder as well as a score placeholder
+
  
  
  
