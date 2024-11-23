a3056a865e7f4be6058470d3ca712664fdd6a7ce
Jeff Yarbrough <jyarbroughiv@gmail.com>
2024-11-23 02:02:23 -0800

    jyarbrough_Hour17and18

---
+++ jyarbrough_Hour17and18/Assets/NOTES.cs
@@ -0,0 +1,4 @@
+/*
+ I chose to use the hour 3 scene because I wanted to try creating multiple controllers. I initially splis the control scripts up and left them that way, 
+ although I am aware that I could combine them into a single script. 
+*/
---
+++ jyarbrough_Hour17and18/Assets/jyarbrough_animation/Scripts/Hover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get reference to animator
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("Hover");
+        }
+    }
+}
+
+
+/*
+ I chose to use the hour 3 scene because I wanted to try creating multiple controllers. I initially splis the control scripts up and left them that way, 
+ although I am aware that I could combine them into a single script. 
+*/
