ba12325f0d963bb67af95ccbe05d5ec524f6a3aa
Jeff Yarbrough <jyarbroughiv@gmail.com>
2024-12-05 11:50:30 -0800

    jyarbrough_Hour20
    Gauntlet runner

---
+++ jyarbrough_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,59 @@
+/*
+1. Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+
+I dont actually believe it's "too hard". However, I believe it can be a little difficult to 
+move from one side to the other in time to obtain a powerup. Also, the phae modes seems to 
+be a little long and the barriers sometimes spawn too close together. 
+
+
+2. What 2 changes would you do to make it easier or harder?
+
+As stated I don't find it particularly hard, nor do I find it simple. 
+So these are some of the things i would consider to bring balance to the game. 
+
+    a. I would like to have a small area that the player model can move forward and back,
+       not just left to right. 
+      
+    b. I would make adjustments to the run speed when running forward and backwards as 
+       well as adjusting the strafe speed. 
+
+    c. I think a separate ability, like jumping or rollong, could be fun. This would require
+       new animations (potentially available on Mixamo). 
+      
+
+3. List 2 things that would give it a "wow" factor.
+    
+    a. Having boost that is stored up vs just adding time to the timer; so an additional powerup.
+
+    b. A good Heads-Up-Display (HUD) would make it feel more complete. 
+        i. Active timer
+       ii. Points system
+      iii. Boost meter
+       iv. Distance Traveled
+    
+    c. Better, more complete background art (a destroyed/post-apocalyptic cityscape)
+    
+
+
+4. What 2 parts of the game are fun? 
+
+This type of game is not my favorite. That being said
+
+    a. phasing is cool, however, cool-down is a little short and the pahased-out mode is a little long
+
+    b. i like to be able to see how long i can make it before my game is over. My record is 35 seconds. 
+
+5. What 2 parts of the game are tedious? 
+  
+    a. Getting from the left to the right in time to grab consecutive powerups. 
+
+    b. the inablilty to deactivate phase-out before it expires on its own. 
+        i. This could also be added to the answers of question 2. 
+ 
+
+
+
+CHANGES MADE:
+1. Changed obstacle from cube to a rough traffic cone i made in Maya
+2. Added Trails to particle emission on powerup and change the color to green
+ */
